4dc110151b1b802b48a320d5f8136c580f390e6c
plaidScientist <emanschneider@gmail.com>
2021-07-29 22:54:07 -0400

    Base Model Class Created for model Ease-of-Acces

---
+++ network/BASE_MODEL_CLASS.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import os
+
+class _Base_Model_Class():
+    def __init__(self, output_dir, name='model', **model_args):
+        self.output_dir = output_dir
+        self.name = name
+        self.model = _build_model(**model_args)
+        self.optimizer = _build_optimizer()
+        self.loss = _build_loss()
+        self.logger = tf.summary.create_file_writer(os.path.join(self.output_dir, 'logs/', '{}/'.format(self.name)))
+    def save_model():
+        self.model.save(os.path.join(self.output_dir, 'saved_models', '{}/'.format(self.name)))
+    def load_model(save_folder, name=None):
+        if name is None:
+            name = self.name
+        if os.path.exists(os.path.join(save_folder, name)):
+            self.model = tf.keras.models.load_model(os.path.join(save_folder, name))
+        else:
+            raise OSError("Model Save Path Not found in Directory")
+    def summary(self):
+        self.model.summary()
+    def _build_model(**args):
+        #to be written over by subclass
+        return tf.keras.Sequential()
+    def _build_optimizer():
+        #to be written over by subclass
+        return tf.keras.optimizers.Optimizer('model_optimizer')
+    def _build_loss():
+        #to be written over by subclass
+        return tf.keras.losses.Loss()
